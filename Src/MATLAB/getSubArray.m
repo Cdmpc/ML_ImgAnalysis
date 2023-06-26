8f03847d3d1c81c7d1d00266c9d3c44cc85dc3d5
Cdmpc <carlosm7.cdmp@gmail.com>
2023-06-26 15:13:50 -0400

    window encapsulation algorithm added.

--- Src/MATLAB/getSubArray.m
+++ Src/MATLAB/getSubArray.m
@@ -1,4 +1,4 @@
-function submatrix = getSubArray(Pupil_Plane, WinSpace, SubSize)
+function [submatrix, NewMaxRow, NewMaxCol] = getSubArray(Pupil_Plane, WinSpace, SubSize)
 %-- GETSUBARRAY: This routine returns a subarray, as a matrix. Using the
 %-- coordinates in from the top-left corner of the window as a starting
 %-- point denoted by Winspace.It scans the window for the maximum pixel 
@@ -12,12 +12,12 @@ function submatrix = getSubArray(Pupil_Plane, WinSpace, SubSize)
     end
     
     %-- ADD ; AFTER DONE TESTING
-    disp("================ [GETTING SUBARRAY SPOT...] ================");
+    %disp("================ [GETTING SUBARRAY SPOT...] ================");
     Window = imcrop(Pupil_Plane, [WinSpace(1), WinSpace(2), WinSpace(3)-1, WinSpace(4)-1]);
     WindowSize = size(Window);
     [MaxVal, I] = max(Window, [], 'all');
     [Max_Row, Max_Col] = ind2sub(size(Window), I);
-    disp("The max pixel in the current window is == " + MaxVal + " at (Row, Col) == (" + Max_Row + ", " + Max_Col + ")");
+    %disp("The max pixel in the current window is == " + MaxVal + " at (Row, Col) == (" + Max_Row + ", " + Max_Col + ")");
     TopLeftRow = Max_Row - floor(SubSize / 2);
     TopLeftCol = Max_Col - floor(SubSize / 2);
     BottomRightRow = SubSize + TopLeftRow - 1;
@@ -28,32 +28,34 @@ function submatrix = getSubArray(Pupil_Plane, WinSpace, SubSize)
     if(TopLeftRow < 1)
         LeftDisp = length(TopLeftRow:1) - 1;
         TopLeftRow = 1;
-        disp("Pushing forward TLX");
+        %disp("Pushing forward TLX");
         BottomRightRow = BottomRightRow + LeftDisp;
     end
     if(TopLeftCol < 1)
         DownDisp = length(TopLeftCol:1) - 1;
         TopLeftCol = 1;
-        disp("Pushing down TLY");
+        %disp("Pushing down TLY");
         BottomRightCol = BottomRightCol + DownDisp;
     end
     if(BottomRightRow > WindowSize(1))
         RightDisp = length(WindowSize(1):BottomRightRow) - 1;
         BottomRightRow = WindowSize(1);
-        disp("Pushing back BRX");
+        %disp("Pushing back BRX");
         TopLeftRow = TopLeftRow - RightDisp;
     end
     if(BottomRightCol > WindowSize(2))
         UpDisp = length(WindowSize(2):BottomRightCol) - 1;
         BottomRightCol = WindowSize(2);
-        disp("Pushing up BRY");
+        %disp("Pushing up BRY");
         TopLeftCol = TopLeftCol - UpDisp;
     end
     
-    disp("The top left coordinate of subarray: (Row, Col) == (" + TopLeftRow + ", " + TopLeftCol + ")");
-    disp("The bottom right coordinate of subarray: (Row, Col) == (" + BottomRightRow + ", " + BottomRightCol + ")");
+    %disp("The top left coordinate of subarray: (Row, Col) == (" + TopLeftRow + ", " + TopLeftCol + ")");
+    %disp("The bottom right coordinate of subarray: (Row, Col) == (" + BottomRightRow + ", " + BottomRightCol + ")");
     
     Row_Range = TopLeftRow:BottomRightRow; Col_Range = TopLeftCol:BottomRightCol;
     %-- ADD ; AFTER DONE TESTING
     submatrix = Window(Row_Range, Col_Range);
+    [Max, LI] = max(submatrix, [], "all");
+    [NewMaxRow, NewMaxCol] = ind2sub(size(submatrix), LI);
 end
---
+++ Src/MATLAB/makeWindowsCalib.m
@@ -0,0 +1,132 @@
+function CalibCenters = makeWindowsCalib(CalibFile, SpotSpacing, Sigma)
+% MAKEWINDOWS --> Creates pitch * pitch windows around a spot in the pupil
+% plane image. It will center boxes around pixels in a calibration image,
+% so that each center is the brightest. These spots are always evenly
+% spaced apart. 
+
+% ARGUEMENTS:
+% CalibFile = The calibration image path.
+% SpotSpacing = How many pixels each spot is spaced apart, both in X and Y.
+% Closeness = How close to the center the window should span out
+
+% RETURNS:
+% CalibCenters = The reference coordinates for the spots, with respect to
+% the whole image.
+% ======================================================================= %
+    CalibMatrix = im2gray(imread(CalibFile));
+    CalibSize = size(CalibMatrix);
+    imshow(CalibMatrix, InitialMagnification="fit");
+    impixelinfo();
+
+    CalibCenters = cat(3);
+    % The pitch of the boxes, should be SpotSpacing / 2, can be a double.
+    Pitch = SpotSpacing / 2;
+
+    % As a starting point, find the entire max pixel in the image and
+    % its location.
+    [~, TotalLI] = max(CalibMatrix, [], "all");
+    [Max_Row, Max_Col] = ind2sub(CalibSize, TotalLI);
+    
+    %-- Given the row and col of total max, subtract them by step to get the first row and column instance
+    % of a possible spot: ROI will be 1/8 of the image dimensions.
+    minRow = Max_Row; minCol = Max_Col;
+    while minRow > floor(CalibSize(1) / Sigma)
+        if(minRow - SpotSpacing <= floor(CalibSize(1) / Sigma))
+            break;
+        else
+            minRow = minRow - SpotSpacing;
+        end
+    end
+    while minCol > floor(CalibSize(2) / Sigma) 
+        if(minCol - SpotSpacing <= floor(CalibSize(2) / Sigma))
+            break;
+        else
+            minCol = minCol - SpotSpacing;
+        end
+    end
+    disp("The first spot will be in (Y, X) = (" + minRow + ", " + minCol + ")"); 
+    TopLeftY = minRow - floor(Pitch / 2);
+    TopLeftX = minCol - floor(Pitch / 2);
+    writematrix([], "SpotDisp.csv", WriteMode="overwrite");
+    
+    currentRow = minRow;
+    currentCol = minCol;
+
+    maxRow = CalibSize(1) - minRow;
+    maxCol = CalibSize(2) - minCol;
+
+    while 1
+        %-- CASE 1: Reached edge in the X direction, the column
+        if(currentCol + SpotSpacing > maxCol && currentRow + SpotSpacing <= maxRow)
+            subimg = imcrop(CalibMatrix, [TopLeftX, TopLeftY, Pitch - 2, Pitch - 2]);
+            [submax, li] = max(subimg, [], "all");
+            [mr, mc] = ind2sub(size(subimg), li);
+            OffSetX = 0; OffSetY = 0;
+            if(mc > floor(Pitch - floor(Pitch / 2)) && currentCol > 1)
+                OffSetX = 1;
+            elseif(mc < floor(Pitch - floor(Pitch / 2)) && currentCol > 1)
+                OffSetX = -1;
+            end
+            if(mr > floor(Pitch - floor(Pitch / 2)) && currentRow > 1)
+                OffSetY = 1;
+            elseif(mr < floor(Pitch - floor(Pitch / 2)) && currentRow > 1)
+                OffSetY = -1;
+            end
+            rectangle(Position=[TopLeftX - 0.5 + OffSetX, TopLeftY - 0.5 + OffSetY, Pitch - 0.5, Pitch - 0.5], EdgeColor="red", LineWidth=1);
+            CalibCell = {currentCol + OffSetX, currentRow + OffSetY, 0, 0};
+            T = cell2table(CalibCell);
+            writetable(T, "SpotDisp.csv", Delimiter=",", WriteMode="append");
+            CalibCenters = cat(3, CalibCenters, [currentRow + OffSetX, currentCol + OffSetY]);
+            currentCol = minCol;
+            currentRow = currentRow + SpotSpacing;
+            TopLeftX = currentCol - floor(Pitch / 2);
+            TopLeftY = currentRow - floor(Pitch / 2);
+        %-- CASE 2: Reached bottom corner, cannot go in X and Y further.
+        elseif(currentRow + SpotSpacing > maxRow && currentCol + SpotSpacing > maxCol)
+            subimg = imcrop(CalibMatrix, [TopLeftX, TopLeftY, Pitch - 2, Pitch - 2]);
+            [submax, li] = max(subimg, [], "all");
+            [mr, mc] = ind2sub(size(subimg), li);
+            OffSetX = 0; OffSetY = 0;
+            if(mc > floor(Pitch - floor(Pitch / 2)) && currentCol > 1)
+                OffSetX = 1;
+            elseif(mc < floor(Pitch - floor(Pitch / 2)) && currentCol > 1)
+                OffSetX = -1;
+            end
+            if(mr > floor(Pitch - floor(Pitch / 2)) && currentRow > 1)
+                OffSetY = 1;
+            elseif(mr < floor(Pitch - floor(Pitch / 2)) && currentRow > 1)
+                OffSetY = -1;
+            end
+            rectangle(Position=[TopLeftX - 0.5 + OffSetX, TopLeftY - 0.5 + OffSetY, Pitch - 0.5, Pitch - 0.5], EdgeColor="red", LineWidth=1);
+            CalibCell = {currentCol + OffSetX, currentRow + OffSetY, 0, 0};
+            T = cell2table(CalibCell);
+            writetable(T, "SpotDisp.csv", Delimiter=",", WriteMode="append");
+            CalibCenters = cat(3, CalibCenters, [currentRow + OffSetX, currentCol + OffSetY]);
+            break;
+        %-- CASE 3: Normal case, space in both X and Y available
+        else
+            subimg = imcrop(CalibMatrix, [TopLeftX, TopLeftY, Pitch - 2, Pitch - 2]);
+            [submax, li] = max(subimg, [], "all");
+            [mr, mc] = ind2sub(size(subimg), li);
+            OffSetX = 0; OffSetY = 0;
+            if(mc > floor(Pitch - floor(Pitch / 2)) && currentCol > 1)
+                OffSetX = 1;
+            elseif(mc < floor(Pitch - floor(Pitch / 2)) && currentCol > 1)
+                OffSetX = -1;
+            end
+            if(mr > floor(Pitch - floor(Pitch / 2)) && currentRow > 1)
+                OffSetY = 1;
+            elseif(mr < floor(Pitch - floor(Pitch / 2)) && currentRow > 1)
+                OffSetY = -1;
+            end
+            rectangle(Position=[TopLeftX - 0.5 + OffSetX, TopLeftY - 0.5 + OffSetY, Pitch - 0.5, Pitch - 0.5], EdgeColor="red", LineWidth=1);
+            CalibCell = {currentCol + OffSetX, currentRow + OffSetY, 0, 0};
+            T = cell2table(CalibCell, VariableNames=["Column #", "Row #", "Vertical Disp", "Horizantal Disp"]);
+            writetable(T, "SpotDisp.csv", Delimiter=",", WriteMode="append");
+            CalibCenters = cat(3, CalibCenters, [currentRow + OffSetX, currentCol + OffSetY]);
+            currentCol = currentCol + SpotSpacing;
+            TopLeftX = currentCol - floor(Pitch / 2);
+        end 
+    end
+    disp("CALIBRATION FILE WRITTEN TO .CSV FILE");
+end
